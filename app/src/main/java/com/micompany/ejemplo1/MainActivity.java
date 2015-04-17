27d0094a2b78c50b8161a4c3624c82934fa85d72
francisquino <francisjmendez@yahoo.es>
2015-04-17 13:37:35 +0200

    Primera subida desde Mérida

---
+++ app/src/main/java/com/micompany/ejemplo1/MainActivity.java
@@ -0,0 +1,43 @@
+package com.micompany.ejemplo1;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+
+public class MainActivity extends ActionBarActivity implements View.OnClickListener {
+
+    TextView mainTextView;
+    Button mainButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        // 1. Access the TextView defined in layout XML
+        // and then set its text
+        mainTextView = (TextView) findViewById(R.id.main_textview);
+        mainTextView.setText("¡Cambiado en el código!");
+        // 2. Access the Button defined in layout XML
+        // and listen for it here
+        mainButton = (Button) findViewById(R.id.main_button);
+        mainButton.setOnClickListener(this);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public void onClick(View v) {
+        // Test the Button
+        mainTextView.setText("¡¡No me toques el botón!!");
+    }
+}
