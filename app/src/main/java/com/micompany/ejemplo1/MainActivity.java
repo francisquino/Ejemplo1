af9d3c3b99a1fc8f630afe88317a534339c1cdb7
francisquino <francisjmendez@yahoo.es>
2015-04-17 14:24:45 +0200

    Segunda subida desde Mérida. Añado EditText y ListView

--- app/src/main/java/com/micompany/ejemplo1/MainActivity.java
+++ app/src/main/java/com/micompany/ejemplo1/MainActivity.java
@@ -4,14 +4,23 @@ import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.TextView;
+
+import java.util.ArrayList;
 
 
 public class MainActivity extends ActionBarActivity implements View.OnClickListener {
 
     TextView mainTextView;
     Button mainButton;
+    EditText mainEditText;
+    ListView mainListView;
+    ArrayAdapter mArrayAdapter;
+    ArrayList mNameList = new ArrayList();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +34,18 @@ public class MainActivity extends ActionBarActivity implements View.OnClickListe
         // and listen for it here
         mainButton = (Button) findViewById(R.id.main_button);
         mainButton.setOnClickListener(this);
+        // 3. Access the EditText defined in layout XML
+        mainEditText = (EditText) findViewById(R.id.main_edittext);
+        // 4. Access the ListView
+        mainListView = (ListView) findViewById(R.id.main_listview);
+
+        // Create an ArrayAdapter for the ListView
+        mArrayAdapter = new ArrayAdapter(this,
+                android.R.layout.simple_list_item_1,
+                mNameList);
+
+        // Set the ListView to use the ArrayAdapter
+        mainListView.setAdapter(mArrayAdapter);
     }
 
 
@@ -37,7 +58,16 @@ public class MainActivity extends ActionBarActivity implements View.OnClickListe
 
     @Override
     public void onClick(View v) {
-        // Test the Button
-        mainTextView.setText("¡¡No me toques el botón!!");
+        //// Test the Button
+        //mainTextView.setText("¡¡No me toques el botón!!");
+
+        // Take what was typed into the EditText
+        // and use in TextView
+        mainTextView.setText("!" + mainEditText.getText().toString()
+                + " está aprendiendo a programar en Android!");
+
+        // Also add that value to the list shown in the ListView
+        mNameList.add(mainEditText.getText().toString());
+        mArrayAdapter.notifyDataSetChanged();
     }
 }
